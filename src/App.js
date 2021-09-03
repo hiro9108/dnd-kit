f5db43324abb4e5ea5ae069d60485b5917ab0a89
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-09-02 19:36:42 -0700

    add: fsort function

--- src/App.js
+++ src/App.js
@@ -1,42 +1,3 @@
-// import "./App.css";
-// import { useState } from "react";
-// import { DndContext, useDroppable } from "@dnd-kit/core";
-// import Droppable from "./components/Droppable";
-// import Draggable from "./components/Draggable";
-
-// function App() {
-//   const [tap, setTap] = useState(false);
-//   const { isOver, setNodeRef } = useDroppable({
-//     id: "droppable",
-//   });
-//   const isOverStyle = {
-//     color: isOver ? "red" : undefined,
-//   };
-
-//   const onCheckHandle = ({ over }) => {
-//     if (!over) return;
-//     setTap(true);
-//   };
-
-//   // const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
-
-//   return (
-//     <DndContext onDragEnd={onCheckHandle}>
-//       <div className="container">
-//         {!tap ? <Draggable id="draggable">Drag me</Draggable> : null}
-
-//         <Droppable>
-//           <div className="drop" ref={setNodeRef} style={isOverStyle}>
-//             {tap ? <Draggable id="draggable">Drag me</Draggable> : "Drop Area"}
-//           </div>
-//         </Droppable>
-//       </div>
-//     </DndContext>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 import { DndContext } from "@dnd-kit/core";
 
@@ -44,29 +5,55 @@ import { Droppable } from "./Droppable";
 import { Draggable } from "./Draggable";
 
 export default function App() {
-  const containers = ["A", "B", "C"];
+  const containers = ["A", "B", "C", "D", "E"];
+
   const [parent, setParent] = useState(null);
-  const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
+
+  // Test: To be able to sort
+  const [containersState, setContainersState] = useState(containers);
+
+  // const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? draggableMarkup : null}
+      {/* {parent === null ? draggableMarkup : null} */}
 
-      {containers.map((id) => (
+      {containersState.map((id) => (
         // We updated the Droppable component so it would accept an `id`
         // prop and pass it to `useDroppable`
         <Droppable key={id} id={id}>
-          {parent === id ? draggableMarkup : "Drop here"}
+          <Draggable id={id}>Drag me {id}</Draggable>
+          {/* {parent === id ? draggableMarkup : "Drop here"} */}
         </Droppable>
       ))}
     </DndContext>
   );
 
   function handleDragEnd(event) {
-    const { over } = event;
+    const { over, active } = event;
+    console.log("active", active);
+    console.log("over", over);
+    console.log("event", event);
 
     // If the item is dropped over a container, set it as the parent
     // otherwise reset the parent to `null`
     setParent(over ? over.id : null);
+
+    // Consider the better algorithm.
+    const activeIndex = containersState.indexOf(active.id);
+    const overIndex = containersState.indexOf(over.id);
+
+    const newContainerArray = containersState.map((el) => {
+      if (el === containersState[activeIndex]) {
+        return containersState[overIndex];
+      }
+      if (el === containersState[overIndex]) {
+        return containersState[activeIndex];
+      }
+      return el;
+    });
+
+    setContainersState(newContainerArray);
+    //
   }
 }
